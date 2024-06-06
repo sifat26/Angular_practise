b51c9379465afa445dd80bbdf845ec8b57ecaf5a
sifat26 <sifatict26@gmail.com>
2024-06-06 22:00:19 +0600

    Service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlogService } from './blog.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   providers: [BlogService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
@@ -16,5 +18,11 @@ export class AppComponent {
   constructor() {
     let service = new BlogService();
     this.blogPosts = service.getBlogPost();
+  }
+  email="sifatict26@gmail.com"
+  onSave()
+  {
+    console.log("Email:"+ this.email);
+
   }
 }

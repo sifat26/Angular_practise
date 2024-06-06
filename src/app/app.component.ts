3fa52e009198c5e2710c60709518233e3b2f4022
sifat26 <sifatict26@gmail.com>
2024-06-06 18:49:48 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogService } from './blog.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [BlogService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular_practise';
+  blogPosts;
+  constructor() {
+    let service = new BlogService();
+    this.blogPosts = service.getBlogPost();
+  }
 }
---
+++ src/app/blog.service.ts
@@ -0,0 +1,35 @@
+export class BlogService{
+    getBlogPost(){
+        return [
+            {
+              "title": "The Future of Technology",
+              "description": "An in-depth look at emerging technologies and their potential impact on society. Explore innovations in AI, blockchain, and more.",
+              "author": {
+                "name": "John Smith"
+              }
+            },
+            {
+              "title": "Healthy Eating: Tips and Recipes",
+              "description": "Learn how to maintain a balanced diet with delicious and nutritious recipes. Tips for healthy eating habits and meal planning included.",
+              "author": {
+                "name": "Emily Johnson"
+              }
+            },
+            {
+              "title": "Mastering the Art of Photography",
+              "description": "A comprehensive guide to improving your photography skills. From beginner tips to advanced techniques, discover how to capture stunning photos.",
+              "author": {
+                "name": "Michael Brown"
+              }
+            },
+            {
+              "title": "Travel Guide: Exploring Europe",
+              "description": "Your ultimate travel guide to the best destinations in Europe. Tips on where to go, what to see, and how to make the most of your trip.",
+              "author": {
+                "name": "Sarah Davis"
+              }
+            }
+          ]
+          
+    }
+}
